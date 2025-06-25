77f9ed8753dba91faf4709eada707408f2e3d605
Cameron Campbell <cameron.campbell325@gmail.com>
2025-06-25 17:12:53 +0100

    fixed bug where macros from indirect derives were not being imported

--- src/rsml_to_model_json.rs
+++ src/rsml_to_model_json.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashMap, fs, path::Path};
+use std::{collections::{HashMap, HashSet}, fs, path::{Path, PathBuf}};
 
 use normalize_path::NormalizePath;
 use rbx_types::{Attributes, Variant};
-use rbx_rsml::{lex_rsml, lex_rsml_derives, lex_rsml_macros, parse_rsml, parse_rsml_derives, parse_rsml_macros, TreeNodeGroup, BUILTIN_MACROS};
+use rbx_rsml::{lex_rsml, lex_rsml_derives, lex_rsml_macros, parse_rsml, parse_rsml_derives, parse_rsml_macros, MacroGroup, TreeNodeGroup, BUILTIN_MACROS};
 use serde::{ser::SerializeStruct, Deserialize, Serialize, Serializer};
 use serde_json::{json, ser::PrettyFormatter, Serializer as JsonSerializer};
 
@@ -119,6 +119,38 @@ fn convert_children(parsed_rsml: &mut TreeNodeGroup, children: Vec<usize>) -> Ve
         .collect::<Vec<Child>>()
 }
 
+fn derive_to_path_buf(derive: &str, parent_path: &Path) -> PathBuf {
+    let derive = if !derive.ends_with(".rsml") { &format!("{}.rsml", derive) } else { derive };
+    parent_path.join(Path::new(derive).normalize())
+}
+
+fn parse_macros_from_derives(
+    derive_path: PathBuf, path: &Path, parent_path: &Path, already_parsed_derives: &mut HashSet<PathBuf>,
+    macro_group: &mut MacroGroup, watcher: &mut WatcherContext
+) {
+    // If the file is valid then we add its macros to the macro group,
+    // then we attempt to add all of the macros from the files dependencies
+    // to the macro group.
+    if let Ok(derive_content) = fs::read_to_string(&derive_path) {
+        parse_rsml_macros(macro_group, &mut lex_rsml_macros(&derive_content));
+
+        let derives = parse_rsml_derives(&mut lex_rsml_derives(&derive_content));
+        for derive in derives {
+            let derive_path = derive_to_path_buf(&derive, parent_path);
+
+            if already_parsed_derives.contains(&derive_path) { continue }
+
+            parse_macros_from_derives(
+                derive_path.clone(), path, parent_path, already_parsed_derives,
+                macro_group, watcher
+            );
+
+            already_parsed_derives.insert(derive_path);
+        }
+    };
+
+    watcher.dependencies.insert(derive_path, path.to_path_buf());
+}
 
 pub fn rsml_to_model_json(path: &Path, watcher: &mut WatcherContext) -> String {
     let parent_path = path.parent().unwrap();
@@ -128,16 +160,16 @@ pub fn rsml_to_model_json(path: &Path, watcher: &mut WatcherContext) -> String {
 
     let derives = parse_rsml_derives(&mut lex_rsml_derives(&content));
 
+    let mut already_parsed_derives: HashSet<PathBuf> = HashSet::new();
+
     let derives_children = derives.iter()
         .map(|derive| {
-            let derive = if !derive.ends_with(".rsml") { &format!("{}.rsml", derive) } else { &derive };
-            let derive_path = parent_path.join(Path::new(derive).normalize());
+            let derive_path = derive_to_path_buf(&derive, parent_path);
 
-            if let Ok(derive_content) = fs::read_to_string(&derive_path) {
-                parse_rsml_macros(&mut macro_group, &mut lex_rsml_macros(&derive_content));
-            };
-
-            watcher.dependencies.insert(derive_path.clone(), path.to_path_buf());
+            parse_macros_from_derives(
+                derive_path.clone(), path, parent_path, &mut already_parsed_derives,
+                &mut macro_group, watcher
+            );
 
             Child::StyleDerive(StyleDerive {
                 name: derive_path.file_stem().unwrap().to_str().unwrap().to_string(),
